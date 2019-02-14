1418c1347ab585644879dccb5fb4776e633f6d45
Olks <26792432+Olks95@users.noreply.github.com>
2019-02-14 12:38:33 +0100

    Make constructor and give SearchBar-sort-options basic functionality

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -1,19 +1,45 @@
 import React from 'react'
 import './SearchBar.css'
 
-let sortByOptions = {
+const sortByOptions = {
 	'Best Match': 'best_match',
 	'Highest Rated': 'rating',
-	'Most Reviewed': 'review_count'
+	'Most Reviewed': 'review_count',
 }
 
+
+
 class SearchBar extends React.Component {
-	
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: '',
+			location: '',
+			sortBy: 'best_match'
+		}
+	}
+
 	renderSortByOptions() {
 		return Object.keys(sortByOptions).map(sortByOption => {
-			let sortByOptionValue = sortByOptions[sortByOption];
-			return <li key={sortByOptionValue}>{sortByOptionValue}</li>
+			const sortByOptionValue = sortByOptions[sortByOption];
+			return <li 
+			key={sortByOptionValue} 
+			className={this.getSortByClass(sortByOptionValue)}
+			onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+			>{sortByOptionValue}</li>
 		});
+	}
+
+	getSortByClass(sortByOption) {
+		if(this.state.sortBy == sortByOption) {
+			return 'active';
+		}
+	}
+
+	handleSortByChange(sortByOption) {
+		this.setState({
+			sortBy: sortByOption
+		})
 	}
 
 	render() {

61e243f9fa9ea1fbb970a34553675b17aa94e2c8
Olks <26792432+Olks95@users.noreply.github.com>
2019-02-14 13:31:35 +0100

    Add handlers for term and location states

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -7,8 +7,6 @@ const sortByOptions = {
 	'Most Reviewed': 'review_count',
 }
 
-
-
 class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
@@ -17,6 +15,8 @@ class SearchBar extends React.Component {
 			location: '',
 			sortBy: 'best_match'
 		}
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
 	}
 
 	renderSortByOptions() {
@@ -42,6 +42,18 @@ class SearchBar extends React.Component {
 		})
 	}
 
+	handleTermChange(event) {
+		this.setState({
+			term: event.target.value
+		})
+	}
+
+	handleLocationChange(event) {
+		this.setState({
+			location: event.target.value
+		})
+	}
+
 	render() {
 		return (
 			<div className="SearchBar">
@@ -51,8 +63,12 @@ class SearchBar extends React.Component {
 			    </ul>
 			  </div>
 			  <div className="SearchBar-fields">
-			    <input placeholder="Search Businesses" />
-			    <input placeholder="Where?" />
+			    <input 
+			    placeholder="Search Businesses"
+			    onChange={this.handleTermChange} />
+			    <input 
+			    placeholder="Where?"
+			    onChange={this.handleLocationChange} />
 			  </div>
 			  <div className="SearchBar-submit">
 			    <a>Let's Go</a>
